5cfaee20e58134a7b87a20cf02ae64e4972ba7c0
Cedric van Putten <me@bycedric.com>
2019-09-04 22:15:27 +0000

    refactor: update proxy for non-interactive mode and help links

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,13 @@
 set -e
 
 if [ -n "$EXPO_CLI_USERNAME" ] && [ -n "$EXPO_CLI_PASSWORD" ]; then
-	expo login --username $EXPO_CLI_USERNAME
+    expo-cli login --non-interactive --username $EXPO_CLI_USERNAME
 else
-	echo 'Skipping Expo login, username and/or password not defined...'
+    echo ''
+    echo 'Skipping automatic Expo login, username and/or password not defined.'
+    echo '  - https://github.com/expo/expo-github-action#used-secrets'
+    echo '  - https://github.com/expo/expo-github-action#example-workflows'
+    echo ''
 fi
 
-sh -c "expo $*"
+sh -c "expo-cli $*"
