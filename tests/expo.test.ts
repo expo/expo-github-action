9f50b4ff6ec92a2f72db2b8d351a08af4525c108
Cedric van Putten <me@bycedric.com>
2019-10-30 20:54:15 +0000

    fix: use cmd suffix when authenticating on windows

--- tests/expo.test.ts
+++ tests/expo.test.ts
@@ -5,6 +5,7 @@ jest.mock('@actions/core', () => core);
 jest.mock('@actions/exec', () => cli);
 
 import * as expo from '../src/expo';
+import { setPlatform, resetPlatform } from './utils';
 
 describe('authenticate', () => {
 	test('skips authentication without credentials', async () => {
@@ -32,5 +33,13 @@ describe('authenticate', () => {
 				EXPO_CLI_PASSWORD: 'mypassword',
 			},
 		});
+	});
+
+	test('executes login command with `.cmd` suffix on windows', async () => {
+		setPlatform('win32');
+		await expo.authenticate('bycedric', 'mypassword');
+		expect(cli.exec).toBeCalled();
+		expect(cli.exec.mock.calls[0][0]).toBe('expo.cmd');
+		resetPlatform();
 	});
 });
