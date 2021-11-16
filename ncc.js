9d4e64ea0e557c109132847bd500b65eba154089
Cedric van Putten <me@bycedric.com>
2021-11-16 13:11:13 +0100

    refactor: upgrade ncc and add multiple entrypoints

---
+++ ncc.js
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const fs = require('fs');
+const ncc = require('@vercel/ncc');
+
+const BUILD_DIR = path.resolve(__dirname, 'build');
+const ACTION_DIR = path.resolve(__dirname, 'src/actions');
+
+const options = {
+	cache: false,
+	minify: true,
+	sourceMap: true,
+	quiet: true,
+};
+
+async function write(file, content) {
+	await fs.promises.writeFile(file, content, { encoding: 'utf-8' });
+}
+
+async function build(file) {
+	const { code, map, assets } = await ncc(file, options);
+	const name = path.basename(file, path.extname(file));
+	const dir = path.resolve(BUILD_DIR, name);
+
+	await fs.promises.mkdir(dir, { recursive: true })
+
+	for (const asset in assets) {
+		await fs.promises.mkdir(path.join(dir, path.dirname(asset)), { recursive: true })
+		await write(path.join(dir, asset), assets[asset].source);
+	}
+
+	await write(path.join(dir, 'index.js'), code);
+	await write(path.join(dir, 'index.map.js'), map);
+
+	console.log(`✓ ${path.relative(__dirname, file)}`);
+}
+
+async function main() {
+	const actionPath = path.resolve(__dirname, './src/actions')
+	const actionFiles = (await fs.promises.readdir(actionPath, { withFileTypes: true }))
+		.filter(entity => entity.isFile())
+		.map(entity => path.join(ACTION_DIR, entity.name));
+
+	return Promise.all(actionFiles.map(build))
+}
+
+main();
