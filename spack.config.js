dbbc1a45a72117d9dc6b2ade3b323a61efd3de8c
Cedric van Putten <me@bycedric.com>
2022-01-12 15:40:57 +0100

    chore: fix linting issues

--- spack.config.js
+++ spack.config.js
@@ -4,11 +4,9 @@ const { resolve, basename, extname } = require('path');
 
 module.exports = config({
   target: 'node',
-  entry: actions(
-    resolve(__dirname, './src/actions'),
-  ),
+  entry: actions(resolve(__dirname, 'src/actions')),
   output: {
-    path: resolve(__dirname, './build'),
+    path: resolve(__dirname, 'build'),
   },
   externalModules: externals(),
 });
@@ -21,20 +19,21 @@ function actions(dir) {
   return Object.fromEntries(
     readdirSync(dir, { withFileTypes: true })
       .filter(entity => entity.isFile())
-      .map(entity => [
-        basename(entity.name, extname(entity.name)),
-        resolve(dir, entity.name),
-      ])
+      .map(entity => {
+        const actionName = basename(entity.name, extname(entity.name));
+        const actionFile = resolve(dir, entity.name);
+        return [actionName, actionFile];
+      })
   );
 }
 
 /**
  * Get all external modules that should not be bundled.
  * Ignoring the packages in the `dependencies` list should keep them up to date.
- * 
+ *
  * Note, currently `@actions/cache` can't be bundled by SWC due to modules issues in `@azure/core-http`:
  *   > internal error: entered unreachable code: module item found but is_es6 is false
- * 
+ *
  * @see https://github.com/expo/expo-github-action/pull/62
  */
 function externals() {
