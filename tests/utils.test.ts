dfd0e848e453c05733f16fbd020dc35f0c89d92e
Cedric van Putten <github@cedric.dev>
2023-02-17 04:37:46 +0100

    test(preview): drop unused test

--- tests/utils.test.ts
+++
@@ -1,7 +0,0 @@
-import { templateLiteral } from '../src/utils';
-
-describe(templateLiteral, () => {
-  it('handles string variables', () => {
-    expect(templateLiteral('Hello, {name}!', { name: 'World' })).toBe('Hello, World!');
-  });
-});
