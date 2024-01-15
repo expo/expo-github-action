b8a9be8336bf5609f8e299a8486f9a2bf7073f8a
Cedric van Putten <me@bycedric.com>
2024-01-15 13:28:27 +0100

    fix(preview): filter slugs with special characters to match dev clients `exp+<slug>` scheme (#252)
    * test(preview): add tests for eas update qr code urls
    
    * fix(preview): use same filtering of `slug` as done in `expo-dev-client`
    
    * chore: rebuild actions

---
+++ tests/eas.test.ts
@@ -0,0 +1,36 @@
+import { getUpdateGroupQr } from '../src/eas';
+
+describe(getUpdateGroupQr, () => {
+  it('returns url for expo-go', () => {
+    expect(
+      getUpdateGroupQr({
+        qrTarget: 'expo-go',
+        projectId: 'projectId',
+        updateGroupId: 'updateGroupId',
+        appSlug: 'appslug',
+      })
+    ).toBe('https://qr.expo.dev/eas-update?projectId=projectId&groupId=updateGroupId');
+  });
+
+  it('returns url for dev-build', () => {
+    expect(
+      getUpdateGroupQr({
+        qrTarget: 'dev-build',
+        projectId: 'projectId',
+        updateGroupId: 'updateGroupId',
+        appSlug: 'appslug',
+      })
+    ).toBe('https://qr.expo.dev/eas-update?appScheme=appslug&projectId=projectId&groupId=updateGroupId');
+  });
+
+  it('returns url for dev-build, with `_` in appSlug', () => {
+    expect(
+      getUpdateGroupQr({
+        qrTarget: 'dev-build',
+        projectId: 'projectId',
+        updateGroupId: 'updateGroupId',
+        appSlug: 'hello_world',
+      })
+    ).toBe('https://qr.expo.dev/eas-update?appScheme=helloworld&projectId=projectId&groupId=updateGroupId');
+  });
+});
